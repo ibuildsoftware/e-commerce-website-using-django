e0357582fadcf48441e559e3ee51938d896ef34d
ibuildsoftware <ibuildsoftware@outlook.fr>
2024-11-25 05:23:29 +0100

    Create a new view and url

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -125,11 +125,11 @@ STATIC_URL = '/static/'
 
 # Configure static files
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles')]
 
 # Configure media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = [os.path.join(BASE_DIR, "media")]
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
