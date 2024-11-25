1e04971437c2d65f4e4c13bfd9ed1e27c62f4d50
ibuildsoftware <ibuildsoftware@outlook.fr>
2024-11-25 05:13:54 +0100

    Fix a bug

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR,'staticfiles')
 
 # Configure media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = [os.path.join(BASE_DIR, "media")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
