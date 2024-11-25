59e4d520ccb34cba07fa442f1348a9b47e2ac1aa
ibuildsoftware <ibuildsoftware@outlook.fr>
2024-11-25 03:48:58 +0100

    Configure static and media files in Settings.py and Urls.py

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -121,7 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Configure static files
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR,'staticfiles')
+
+# Configure media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
