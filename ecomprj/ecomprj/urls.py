e781f6cd0cb4e12b2c25253067ce52b317c4a709
ibuildsoftware <ibuildsoftware@outlook.fr>
2024-11-25 07:11:52 +0100

    Create a new Urls.py file inside 'core' folder

--- ecomprj/ecomprj/urls.py
+++ ecomprj/ecomprj/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
+#from core.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", index)
+    #path("", index)
 ]
