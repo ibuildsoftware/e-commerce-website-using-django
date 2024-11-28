73acd60902d87dc0688a74d55cb8b3d7859e72ab
ibuildsoftware <ibuildsoftware@outlook.fr>
2024-11-28 06:47:09 +0100

    Configure template inheritance and partials

--- ecomprj/ecomprj/urls.py
+++ ecomprj/ecomprj/urls.py
@@ -18,8 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 #from core.views import index
 
+# for static and media files
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("core.urls"))
 ]
 
+# for static and media files
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
